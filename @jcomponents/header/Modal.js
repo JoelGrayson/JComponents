0aa4613eef0ff7e5ed6243b9fd78b956703f77ba
Joel Grayson <joel@joelgrayson.com>
2022-08-04 15:27:43 -0400

    Modal is draggable

---
+++ @jcomponents/header/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './modal.css';
+import './icon-close.css';
+
+export default function Modal({open, setOpen, children, ...props}) {
+    return (<>
+        { open && (<>
+            <div className='modal-background' onClick={_=>setOpen(false)}/> {/* Clicking outside of a modal closes it */}
+            <div className='modal-container'>
+                <div className='settings-modal'
+                    style={{ maxWidth: props.maxWidth || "500px" }}
+                    onClick={e=>{
+                        console.log(e);
+                    }}
+                >
+                    <span className='icon-close' onClick={_=>setOpen(false)} />
+                    <div className='settings-modal-content' onClick={e=>e.stopPropagation()} >
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </>)}
+    </>);
+}
--- @jcomponents/modal/Modal.js
+++ @jcomponents/modal/Modal.js
@@ -1,46 +1,23 @@
 import React from 'react';
+import './modal.css';
 import './icon-close.css';
 
 export default function Modal({open, setOpen, children, ...props}) {
     return (<>
         { open && (
             <>
-                <div id='modal-background' onClick={_=>setOpen(false)}
-                    style={{
-                        position: "fixed",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "100%",
-                        backgroundColor: "rgba(0.1, 0.1, 0.1, 0.5)",
-                        zIndex: 1
-                    }}
-                /> {/* Clicking outside of a modal closes it */}
-                <div id='modal-container'
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "100%",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}
-                >
-                    <div id='settings-modal' style={{
-                        position: "fixed",
-                        width: "80%",
-                        maxWidth: props.maxWidth || "500px",
-                        height: "fit-content",
-                        backgroundColor: "white",
-                        zIndex: 2,
-                        padding: "30px",
-                        paddingBottom: "40px",
-                        borderRadius: "1.4rem"
-                    }}>
+                <div className='modal-background' onClick={_=>setOpen(false)}/> {/* Clicking outside of a modal closes it */}
+                <div className='modal-container'>
+                    <div className='settings-modal'
+                        style={{
+                            maxWidth: props.maxWidth || "500px",
+                        }}
+                        onClick={e=>{
+                            console.log(e);
+                        }}
+                    >
                         <span className='icon-close' onClick={_=>setOpen(false)} />
-                        <div id='settings-modal-content'>
+                        <div className='settings-modal-content' onClick={e=>e.stopPropagation()}>
                             {children}
                         </div>
                     </div>
