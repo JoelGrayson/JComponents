f45d9258878fbe216086f3a21e0ae86c5409f219
Joel Grayson <joel@joelgrayson.com>
2022-08-04 18:10:30 -0400

    Fixed @jcomponents/button problem

--- @jcomponents/button/Button.js
+++ @jcomponents/button/Button.js
@@ -5,11 +5,12 @@ export default function Button({children, color, ...props}) {
     // Color from the j defaults or any CSS valid color
     const validJColors=['jred', 'jyellow', 'jgreen', 'jblue'];
     const validColor=validJColors.includes(color.toLowerCase());
+
+    const style=validColor ? false : {} //if valid color, ignore style attribute, else use CSS color
+    
     return (<button
         className={validColor ? `jcomponent-button ${color}` : 'jcomponent-button'}
-        style={{
-            backgroundColor: validColor ? '' : color //if not valid Color, then use CSS color
-        }}
+        style={style}
         {...props}
     >
         {children}
