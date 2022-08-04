1ac9c2c0397b31bb544eba088ec658500bea5418
Joel Grayson <joel@joelgrayson.com>
2022-08-04 17:27:51 -0400

    Choose any button color: a jdefault or CSS color

--- @jcomponents/button/Button.js
+++ @jcomponents/button/Button.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './jcomponent-button.css';
 
-export default function Button({children, ...props}) {
-    return (<button className='jcomponent-button' {...props}>{children}</button>);
+export default function Button({children, color, ...props}) {
+    // Color from the j defaults or any CSS valid color
+    const validJColors=['jred', 'jyellow', 'jgreen', 'jblue'];
+    if (validJColors.includes(color.toLowerCase()))
+        return (<button className={`jcomponent-button ${color}`} {...props}>
+            {children}
+        </button>);
+    else
+        return (<button className='jcomponent-button' style={{
+            backgroundColor: color
+        }} {...props}>
+            {children}
+        </button>);
 }
