1e9c1593dcd297cb7e114a1b734a3c08fdfd7bb3
Joel Grayson <joel@joelgrayson.com>
2022-09-27 08:33:34 -0400

    🐛 NavItem bug fix

--- @jcomponents/nav/Nav.tsx
+++ @jcomponents/nav/Nav.tsx
@@ -2,26 +2,31 @@ import React, { useState, useEffect } from 'react';
 import Button from '@jcomponents/button';
 
 export default class Nav extends React.Component<{ gap: number; [x: string]: any }, any> {
-    static Item=function() {
+    static Item=function(props: {
+        children: any;
+        href?: string;
+        Link?: any; //next/link
+        [key: string]: any;
+    }) {
         const [isCurrentPage, setIsCurrentPage] = useState(false);
-        const href=this.props.href ?? '#'
-        const Link=this.props.Link ?? null
-        
+        const href=props.href ?? '#'
+        const Link=props.Link ?? null
+
         useEffect(()=>{
             if (typeof window!=='undefined')
                 setIsCurrentPage(href===window.location.pathname);
         }, []);
         
-        if (Link==null)
+        if (!Link)
             return (<li style={{padding: 0, listStyleType: 'none'}}>
                 <a href={href}>
-                    <Button color={isCurrentPage ? 'jyellow' : ''}>{this.props.children}</Button>
+                    <Button color={isCurrentPage ? 'jyellow' : ''}>{props.children}</Button>
                 </a>
             </li>);
         else
             return (<li style={{padding: 0, listStyleType: 'none'}}>
                 <Link href={href}><a>
-                    <Button color={isCurrentPage ? 'jyellow' : ''}>{this.props.children}</Button>
+                    <Button color={isCurrentPage ? 'jyellow' : ''}>{props.children}</Button>
                 </a></Link>
             </li>);
     }
