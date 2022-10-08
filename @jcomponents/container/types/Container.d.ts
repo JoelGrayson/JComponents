4b66fa48c0dc4ea31be9de966eee0fd00a67e51b
Joel Grayson <joel@joelgrayson.com>
2022-10-08 17:00:21 -0400

    Fix container bug

--- @jcomponents/container/types/Container.d.ts
+++ @jcomponents/container/types/Container.d.ts
@@ -1,5 +1,5 @@
 declare module '@jcomponents/container' {
     import React from 'react';
     
-    export default function Container({children, ...props}: { children: any, [key: string]: value }): JSX.Element;
+    export default function Container({children, ...props}: { children: any; [key: string]: any; }): JSX.Element;
 }
