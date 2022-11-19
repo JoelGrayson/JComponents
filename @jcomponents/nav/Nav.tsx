9fa85f90b990c0554eca6e635d3d26d400efa850
Joel Grayson <joel@joelgrayson.com>
2022-11-19 17:48:18 -0500

    Bug fix Nav

--- @jcomponents/nav/Nav.tsx
+++ @jcomponents/nav/Nav.tsx
@@ -2,36 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Button from '@jcomponents/button';
 
 export default class Nav extends React.Component<{ gap?: number; [x: string]: any }, any> {
-    static Item=function(props: {
-        children: any;
-        href?: string;
-        Link?: any; //next/link
-        [key: string]: any;
-    }) {
-        const [isCurrentPage, setIsCurrentPage] = useState(false);
-        const href=props.href ?? '#';
-        const Link=props.Link ?? null;
-
-        useEffect(()=>{
-            if (typeof window!=='undefined')
-                setIsCurrentPage(href===window.location.pathname);
-        }, []);
-
-        if (!Link)
-            return (<li style={{padding: 0, listStyleType: 'none'}}>
-                <a href={href}>
-                    <Button color={isCurrentPage ? 'jyellow' : ''}>{props.children}</Button>
-                </a>
-            </li>);
-        else
-            return (<li style={{padding: 0, listStyleType: 'none'}}>
-                <Link href={href}><a className='unstyled'>
-                    <Button color={isCurrentPage ? 'jyellow' : ''}>{props.children}</Button>
-                </a></Link>
-            </li>);
-    }
-
-    render() {
+    render(): JSX.Element { //<Nav>
         const gap=this.props.gap ?? 4;
         
         return (<nav>
@@ -43,5 +14,35 @@ export default class Nav extends React.Component<{ gap?: number; [x: string]: an
                 {this.props.children}
             </ul>
         </nav>);
+    }
+
+    static Item=function(props: { // <Nav.Item>
+        children: any;
+        href?: string;
+        Link?: any; //next/link
+        [key: string]: any;
+    }): JSX.Element {
+        const [btnColor, setBtnColor]=useState<'' | 'jyellow'>(''); //if is current page, set to jyellow
+        const href=props.href ?? '#';
+        const Link=props.Link ?? null;
+
+        useEffect(()=>{
+            if (typeof window!=='undefined')
+                if (href===window.location.pathname)
+                    setBtnColor('jyellow');
+        }, []);
+
+        if (!Link)
+            return (<li style={{padding: 0, listStyleType: 'none'}}>
+                <a href={href}>
+                    <Button color={btnColor}>{props.children}</Button>
+                </a>
+            </li>);
+        else
+            return (<li style={{padding: 0, listStyleType: 'none'}}>
+                <Link href={href}><a className='unstyled'>
+                    <Button color={btnColor}>{props.children}</Button>
+                </a></Link>
+            </li>);
     }
 }
