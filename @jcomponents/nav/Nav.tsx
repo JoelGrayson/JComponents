3cc6bcc86912e1de01161271e1fb5ca4dd63cc04
Joel Grayson <joel@joelgrayson.com>
2022-12-28 17:04:24 -0500

    Avoid using next/link's <Link>

--- @jcomponents/nav/Nav.tsx
+++ @jcomponents/nav/Nav.tsx
@@ -16,33 +16,23 @@ export default class Nav extends React.Component<{ gap?: number; [x: string]: an
         </nav>);
     }
 
-    static Item=function(props: { //<Nav.Item>
+    static Item=function({children, href='#', target, ...props}: { //<Nav.Item>
         children: any;
         href?: string;
-        Link?: any; //next/link
         target?: '_blank' | '_self' | '_parent' | '_top' | string;
         [key: string]: any;
     }): JSX.Element {
         const [btnColor, setBtnColor]=useState<'jnone' | 'jyellow'>('jnone'); //if is current page, set to jyellow. Has to be 'jnone' so it is not null
-        const href=props.href ?? '#';
-        const Link=props.Link ?? null;
 
         useEffect(()=>{
             if (href===window.location.pathname)
                 setBtnColor('jyellow');
         }, []);
         
-        if (!Link)
-            return (<li style={{padding: 0, listStyleType: 'none'}}>
-                <a href={href} target={props.target}>
-                    <Button color={btnColor}>{props.children}</Button>
-                </a>
-            </li>);
-        else
-            return (<li style={{padding: 0, listStyleType: 'none'}}>
-                <Link href={href} target={props.target}>
-                    <Button color={btnColor}>{props.children}</Button>
-                </Link>
-            </li>);
+        return (<li style={{padding: 0, listStyleType: 'none'}}>
+            <a className='unstyled' href={href} target={target} {...props}>
+                <Button color={btnColor}>{children}</Button>
+            </a>
+        </li>);
     }
 }
