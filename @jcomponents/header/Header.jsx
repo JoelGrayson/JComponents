5754682456d9d2b91c359bb43a8e7efd6370337b
Joel Grayson <joel@joelgrayson.com>
2023-03-24 16:53:54 -0400

    header bug fix

--- @jcomponents/header/Header.jsx
+++ @jcomponents/header/Header.jsx
@@ -8,13 +8,13 @@ export default function Header({maxWidth=800, color, ...props}) {
         const buff=tinycolor(color || '#a1c7ef');
         topColor=buff.toString();
         bottomColor=buff.lighten(8).toString();
-    } else if (Array.isArray(color)) {
+    } else if (typeof color==='object' && color.from && color.to) {
         topColor=color.from;
         bottomColor=color.to;
     }
     
     return (<header className='jcomponents-header' style={{
-        backgroundImage: `linear-gradient(0, ${topColor}, ${bottomColor})`
+        backgroundImage: `linear-gradient(${topColor}, ${bottomColor})`
     }}>
         <div style={{ maxWidth }}>
             {props.children}
