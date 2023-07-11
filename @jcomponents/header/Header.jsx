76348db5c51c7f525baea8b66c2983c731c6971d
Joel Grayson <joel@joelgrayson.com>
2023-07-10 21:13:46 -0400

    header color optional (default color)

--- @jcomponents/header/Header.jsx
+++ @jcomponents/header/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import tinycolor from 'tinycolor2';
 import './header.css';
 
-export default function Header({maxWidth=800, color, ...props}) {
+export default function Header({maxWidth=800, color='#a1c7ef', ...props}) {
     let topColor, bottomColor;
-    if (!color || typeof color==='string') {
-        const buff=tinycolor(color || '#a1c7ef');
+    if (typeof color==='string') {
+        const buff=tinycolor(color);
         topColor=buff.toString();
         bottomColor=buff.lighten(8).toString();
     } else if (typeof color==='object' && color.from && color.to) {
@@ -13,11 +13,12 @@ export default function Header({maxWidth=800, color, ...props}) {
         bottomColor=color.to;
     }
     
-    return (<header className='jcomponents-header' style={{
+    return <header className='jcomponents-header' style={{
         backgroundImage: `linear-gradient(${topColor}, ${bottomColor})`
     }}>
         <div style={{ maxWidth }}>
             {props.children}
         </div>
-    </header>);
+    </header>;
 }
+
