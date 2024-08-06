4447340d337c5d6deeb9f8f2e378fe82d7977798
Joel Grayson <joel@joelgrayson.com>
2024-08-06 17:00:02 -0400

    color and background color configuration

--- @jcomponents/fancyol/FancyOl.tsx
+++ @jcomponents/fancyol/FancyOl.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 
-export default function FancyOl({ children, spacing=5, color='#1d2cf3' }: { children: React.ReactNode; spacing?: number; color?: string; }) {
+export default function FancyOl({ children, spacing=5, color='white', backgroundColor='#1d2cf3' }: { children: React.ReactNode; spacing?: number; color?: string; backgroundColor?: string }) {
     const childrenArray=React.Children.toArray(children);
 
     return <div className="grid grid-cols-[32px_1fr]">
         {childrenArray.map((child, i)=>{
             return <React.Fragment key={i}>
-                <div className="unstyled relative">
+                <div className="unstyled" style={{ position: 'relative' }}>
                     <div
-                        className={`rounded-full p-3 text-white font-bold h-5 w-5 flex justify-center items-center ${i==childrenArray.length-1 ? '' : 'after:h-full after:w-1 after:absolute after:bg-[#1d2cf3] after:top-1'}`}
                         style={{
-                            backgroundColor: '#1d2cf3'
+                            borderRadius: '50%',
+                            fontWeight: 700,
+                            padding: '0.75rem',
+                            height: '1.25rem',
+                            width: '1.25rem',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            color,
+                            backgroundColor,
                         }}
                     >
                         <span className="relative top-[1px] z-[1]">{i+1}</span>
+                        {/* After */}
+                        { i==childrenArray.length-1 && <div
+                            style={{
+                                height: '100%',
+                                width: '4px',
+                                position: 'absolute',
+                                backgroundColor,
+                                top: '0.25px',
+                            }}
+                        /> }
                     </div>
                 </div>
                 <div className="unstyled" style={{ marginBottom: spacing }}>
--- @jcomponents/fancyol/fancyol.d.ts
+++ @jcomponents/fancyol/fancyol.d.ts
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function FancyOl({ children, spacing, color }: {
+export default function FancyOl({ children, spacing, color, backgroundColor }: {
     children: React.ReactNode;
     spacing?: number;
     color?: string;
+    backgroundColor?: string;
 }): React.JSX.Element
+
